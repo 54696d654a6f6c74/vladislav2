c918304a66d6ca69c7eae8f2b7929fcf49561051
bitwise <bit@localhost.localdomain>
2023-08-10 01:03:29 +0100

    Ignore output dir when walking

--- src/walker.rs
+++ src/walker.rs
@@ -2,30 +2,29 @@ use crate::settings::Settings;
 use walkdir::{DirEntry, WalkDir};
 
 fn is_excluded(file: &DirEntry, settings: &Settings) -> bool {
-    if let Some(except_dir) = settings.except_dir.as_ref() {
-        let path_components = file.path().components();
+    let file_path = file.path();
 
-        for component in path_components {
+    for component in file_path.components() {
+        if String::from(component.as_os_str().to_str().unwrap_or("")).eq(&settings.output_dir) {
+            return true;
+        }
+    }
+    if let Some(except_dir) = settings.except_dir.as_ref() {
+        for component in file_path.components() {
             if except_dir.contains(&String::from(component.as_os_str().to_str().unwrap_or(""))) {
                 return true;
             }
         }
     }
-    if settings
-        .except_path
-        .as_ref()
-        .unwrap()
-        .contains(&file.path().to_str().unwrap().to_string())
-    {
-        return true;
+    if let Some(except_path) = settings.except_path.as_ref() {
+        if except_path.contains(&file_path.to_str().unwrap().to_string()) {
+            return true;
+        }
     }
-    if settings
-        .except_filename
-        .as_ref()
-        .unwrap()
-        .contains(&file.file_name().to_str().unwrap_or_default().to_string())
-    {
-        return true;
+    if let Some(except_filename) = settings.except_filename.as_ref() {
+        if except_filename.contains(&file.file_name().to_str().unwrap_or_default().to_string()) {
+            return true;
+        }
     }
 
     return false;
@@ -33,25 +32,27 @@ fn is_excluded(file: &DirEntry, settings: &Settings) -> bool {
 
 fn is_eligible(file: &DirEntry, settings: &Settings) -> bool {
     fn get_file_ext(file: &DirEntry) -> &str {
-        file.file_name()
+        return file
+            .file_name()
             .to_str()
             .unwrap_or_default()
             .split('.')
             .by_ref()
             .last()
-            .unwrap_or_default()
+            .unwrap_or_default();
     }
 
-    file.file_type().is_file() && settings.file_ext.eq(get_file_ext(file))
+    return file.file_type().is_file() && settings.file_ext.eq(get_file_ext(file));
 }
 
 fn is_hidden(file: &DirEntry) -> bool {
-    file.path()
+    return file
+        .path()
         .file_name()
         .unwrap_or_default()
         .to_str()
-        .map(|s| s.starts_with('.'))
-        .unwrap_or(false)
+        .map(|s| return s.starts_with('.'))
+        .unwrap_or(false);
 }
 
 pub fn walk(settings: &Settings) -> Vec<DirEntry> {
